67f910cdaa8d66c1127fdf4825cae5e824215a12
Jesper Svensson <svejes@gmail.com>
2015-03-12 15:43:19 +0100

    Add check to prevent duplicate submits based upon the shippment type, corrected lint-errors

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,4 @@
+/*global localStorage: false, console: false, $: false, chrome: false, XMLHttpRequest: false, window: false */
 
 var popup = {
 
@@ -40,11 +41,13 @@ var popup = {
   * 
   */
   selectShippment: function () {
-    var service = localStorage['dhl_service'],
-      code = 'document.getElementsByName("templatePK")[0].value = "'+ service +'";'+       
-          'document.forms[0].submit();document.forms[0].templatePK.disabled=true;';
-    
-    chrome.tabs.executeScript(null, {code: code}, function() {
+    var service = localStorage.dhl_service,
+      code = 'if (document.getElementsByName("templatePK")[0].value != "' + service + '") {' +
+        'document.getElementsByName("templatePK")[0].value = "' + service + '";' +
+        'document.forms[0].submit();document.forms[0].templatePK.disabled=true;' +
+        '}';
+
+    chrome.tabs.executeScript(null, {code: code}, function () {
       this.log("selectShippment: executeScript response received");
     });
   },
@@ -57,10 +60,7 @@ var popup = {
   */
   loadCustomers: function () {
 
-    var request = new XMLHttpRequest(),
-        headers = localStorage['headers'],
-        json_headers = JSON.parse(headers),
-        i;
+    var request = new XMLHttpRequest(), headers = localStorage.headers, json_headers = JSON.parse(headers), i;
 
     this.log("Entering loadCustomers");
 
@@ -73,12 +73,12 @@ var popup = {
     $('#phone').val('');
     $('#email').val('');
 
-    var endpoint = localStorage['endpoint'];
+    var endpoint = localStorage.endpoint;
 
     if (endpoint.length === 0) {
-        var msg = chrome.i18n.getMessage("noEndpointConfiguredText");
-        this.error(msg);
-        return;
+      var msg = chrome.i18n.getMessage("noEndpointConfiguredText");
+      this.error(msg);
+      return;
     }
 
     this.log("Endpoint is " + endpoint + " opening XMLHttpRequest");
@@ -87,8 +87,8 @@ var popup = {
 
     this.log("Adding headers " + headers);
 
-    for (i=0; i<json_headers.length; i++){
-      request.setRequestHeader(json_headers[i].key,json_headers[i].value);
+    for (i = 0; i < json_headers.length; i++) {
+      request.setRequestHeader(json_headers[i].key, json_headers[i].value);
     }
 
     request.onload = this.createCustomerOptions.bind(this);
@@ -101,9 +101,7 @@ var popup = {
   * Populate the customer data in the related fields
   */
   populateCustomerData: function () {
-    
-    var customerId = $('#customers').val(),
-        customer = this.getCustomerById(customerId);
+    var customerId = $('#customers').val(), customer = this.getCustomerById(customerId);
 
     this.log("Entering populateCustomerData");
 
@@ -126,71 +124,49 @@ var popup = {
   */
   fillDhlForm: function () {
 
-    var id = $('#id').val(),
-        name = $('#name').val(),
-        address = $('#address').val(),
-        zipcode = $('#zipcode').val(),
-        city = $('#city').val(),
-        phone = $('#phone').val(),
-        email = $('#email').val(),
-        our_reference = localStorage['our_reference'],
-        goods_item = localStorage['goods_item'];
-    
+    var id = $('#id').val(), name = $('#name').val(), address = $('#address').val(), zipcode = $('#zipcode').val(), city = $('#city').val(), phone = $('#phone').val(), email = $('#email').val(), our_reference = localStorage.our_reference, goods_item = localStorage.goods_item, code;
     this.getTabUrl();
 
     this.log("Entering fillDhlForm");
 
-    this.log("fillDhlForm data is: ");
-    this.log("id: " + id);
-    this.log("name: " + name);
-    this.log("address: " + address);
-    this.log("zipcode: " + zipcode);
-    this.log("city: " + city);
-    this.log("phone: " + phone);
-    this.log("email: " + email);
-  
     if (this.target_urls.register_shipment === this.current_url) {
-
-      var code = 'var evt = document.createEvent("MouseEvents");'+
-        'evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null); '+
-        'var cb = document.getElementById("consignee_mode_other"); '+
-        'cb.dispatchEvent(evt);'+
-        'document.getElementById("consignee_temp.id").value = "' + id +'";'+
-        'document.getElementById("consignee_temp.name").value = "' + name +'";'+
-        'document.getElementById("consignee_temp.addressline[0]").value = "' + address +'";'+
-        'document.getElementById("consignee_temp.postcode").value = "' + zipcode +'";'+
-        'document.getElementById("consignee_temp.city").value = "' + city +'";'+
-        'document.getElementById("consignee_temp.contactPerson").value = "' + name +'";'+
-        'document.getElementById("consignee_temp.mobile").value = "' + phone +'";'+
-        'document.getElementById("consignee_temp.phone").value = "' + phone +'";'+
-        'document.getElementById("consignee_temp.email").value = "' + email +'";'+
-        'document.getElementsByName("consignee_temp_save")[0].checked = true;'+
-        'document.getElementById("orderNo").value = "'+ our_reference +'";'+
-        'document.getElementById("consigneeReference").value = "'+ name +'";'+
-        'document.getElementById("goodsItemList.goodsItems[0].goodsItemReferences[1].value").value="'+ goods_item +'";'+
-        'var evt2 = document.createEvent("MouseEvents");'+
-        'evt2.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null); '+
-        'document.getElementsByName("notify")[0].dispatchEvent(evt2);'+
-        'document.getElementById("notifySmsValue").value = "'+ phone +'";';
+      code = 'var evt = document.createEvent("MouseEvents");' +
+        'evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null); ' +
+        'var cb = document.getElementById("consignee_mode_other"); ' +
+        'cb.dispatchEvent(evt);' +
+        'document.getElementById("consignee_temp.id").value = "' + id + '";' +
+        'document.getElementById("consignee_temp.name").value = "' + name + '";' +
+        'document.getElementById("consignee_temp.addressline[0]").value = "' + address + '";' +
+        'document.getElementById("consignee_temp.postcode").value = "' + zipcode + '";' +
+        'document.getElementById("consignee_temp.city").value = "' + city + '";' +
+        'document.getElementById("consignee_temp.contactPerson").value = "' + name + '";' +
+        'document.getElementById("consignee_temp.mobile").value = "' + phone + '";' +
+        'document.getElementById("consignee_temp.phone").value = "' + phone + '";' +
+        'document.getElementById("consignee_temp.email").value = "' + email + '";' +
+        'document.getElementsByName("consignee_temp_save")[0].checked = true;' +
+        'document.getElementById("orderNo").value = "' + our_reference + '";' +
+        'document.getElementById("consigneeReference").value = "' + name + '";' +
+        'document.getElementById("goodsItemList.goodsItems[0].goodsItemReferences[1].value").value="' + goods_item + '";' +
+        'var evt2 = document.createEvent("MouseEvents");' +
+        'evt2.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null); ' +
+        'document.getElementsByName("notify")[0].dispatchEvent(evt2);' +
+        'document.getElementById("notifySmsValue").value = "' + phone + '";';
     } else {
-
-      var code = 'document.getElementById("id").value = "' + id +'";'+
-        'document.getElementById("name").value = "' + name +'";'+
-        'document.getElementById("addressline.0").value = "' + address +'";'+
-        'document.getElementById("postcode").value = "' + zipcode +'";'+
-        'document.getElementById("city").value = "' + city +'";'+
-        'document.getElementById("contactPerson").value = "' + name +'";'+
-        'document.getElementById("phone").value = "' + phone +'";'+
-        'document.getElementById("mobile").value = "' + phone +'";'+
-        'document.getElementById("email").value = "' + email +'";'+
-        'document.getElementById("type.consignee").checked = true;'+
+      code = 'document.getElementById("id").value = "' + id + '";' +
+        'document.getElementById("name").value = "' + name + '";' +
+        'document.getElementById("addressline.0").value = "' + address + '";' +
+        'document.getElementById("postcode").value = "' + zipcode + '";' +
+        'document.getElementById("city").value = "' + city + '";' +
+        'document.getElementById("contactPerson").value = "' + name + '";' +
+        'document.getElementById("phone").value = "' + phone + '";' +
+        'document.getElementById("mobile").value = "' + phone + '";' +
+        'document.getElementById("email").value = "' + email + '";' +
+        'document.getElementById("type.consignee").checked = true;' +
         'document.getElementById("type.notify").checked = true;';
     }
-    
     this.log("target_url: " + this.current_url + ", code: " + code);
-
-    chrome.tabs.executeScript(null, {code: code}, function() {
-        this.log("executeScript response received");
+    chrome.tabs.executeScript(null, {code: code}, function () {
+      this.log("executeScript response received");
     });
   },
 
@@ -204,8 +180,8 @@ var popup = {
 
     this.log("Entering getTabUrl");
 
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-        _this.current_url = tabs[0].url;
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+      _this.current_url = tabs[0].url;
     });
   },
 
@@ -217,10 +193,10 @@ var popup = {
     * @param {number} customerId, a customer ID
     * @return {JSONobject} customer
     */
-  getCustomerById: function( id ){
-    this.log("Entering getCustomerById", id );
+  getCustomerById: function (id) {
+    this.log("Entering getCustomerById", id);
 
-    return this.customers[ id ];
+    return this.customers[id];
   },
 
   /**
@@ -234,24 +210,19 @@ var popup = {
    * @param {Number|String} customers.id
    * @param {String} customers.name
    */
-  updateCustomerOptionsDOM: function( customers ){
-    
-    var length, 
-        options, 
-        customer, 
-        select_customer_msg = chrome.i18n.getMessage("selectCustomerText");
+  updateCustomerOptionsDOM: function (customers) {
+    var options, customer, select_customer_msg = chrome.i18n.getMessage("selectCustomerText");
 
     this.log("Entering updateCustomerOptionsDOM");
 
-    options = '<option value="">'+ select_customer_msg +'</option>';
-    length = customers.length;
+    options = '<option value="">' + select_customer_msg + '</option>';
 
-    Object.keys( customers ).forEach( function( key ){
-      customer = customers[ key ];
-      options += '<option value="'+ customer.id +'">'+ customer.name +'</option>';
+    Object.keys(customers).forEach(function (key) {
+      customer = customers[key];
+      options += '<option value="' + customer.id + '">' + customer.name + '</option>';
     });
 
-    this.log( 'options are', options );
+    this.log('options are', options);
 
     $('#customers').html(options);
   },
@@ -265,17 +236,15 @@ var popup = {
    * @return {Object} An object with customer ids as keys and the corresponding
    * customer as value.
    */
-  parseResposetextToCustomers: function( responseText ){
-    
+  parseResposetextToCustomers: function (responseText) {
     var customerArray, customers;
-
     this.log("Entering parseResposetextToCustomers");
 
-    customerArray = JSON.parse( responseText );
-    customers = {}
+    customerArray = JSON.parse(responseText);
+    customers = {};
 
-    customerArray.forEach( function( customer ){
-      customers[ customer.id ] = customer;
+    customerArray.forEach(function (customer) {
+      customers[customer.id] = customer;
     });
 
     return customers;
@@ -288,15 +257,14 @@ var popup = {
    *
    * @param {ProgressEvent} e The XHR ProgressEvent.
    */
-  createCustomerOptions: function( event ){
-    
+  createCustomerOptions: function (event) {
     var responseText, customers;
 
     this.log("Entering createCustomerOptions");
 
     responseText = event.target.responseText;
-    customers = this.parseResposetextToCustomers( responseText );
-    this.updateCustomerOptionsDOM( customers );
+    customers = this.parseResposetextToCustomers(responseText);
+    this.updateCustomerOptionsDOM(customers);
     this.customers = customers;
   },
 
@@ -307,13 +275,10 @@ var popup = {
   *
   * @param {string} message, the message to be displayed
   */
-  error: function(message) {
-      
-      var errorContainer = $('#error');
-
-      this.log("Error: " + message);
-
-      errorContainer.html(message);
+  error: function (message) {
+    var errorContainer = $('#error');
+    this.log("Error: " + message);
+    errorContainer.html(message);
   },
 
   /**
@@ -324,12 +289,12 @@ var popup = {
   *
   * @param {string} message, the message to be logged
   */
-  log: function(message) {
-      var debug = localStorage['debug'];
-      
-      if (debug === 'true') {
-          window.console.log(message);
-      }
+  log: function (message) {
+    var debug = localStorage.debug;
+
+    if (debug === 'true') {
+      window.console.log(message);
+    }
   }
 
 };
