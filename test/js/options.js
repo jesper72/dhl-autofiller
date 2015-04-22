b6958ea02732a1852f07adab98dd8d16151cbc7c
Jesper Svensson <svejes@gmail.com>
2015-04-22 13:08:10 +0200

    Change syntax to limit the global scope to QUnit. Remove lint errors

--- test/js/options.js
+++ test/js/options.js
@@ -1,52 +1,51 @@
-
+/*global QUnit: false, options: false, localStorage: false */
 module('options');
 
+QUnit.test('loadOptions', function (assert) {
+  /* Set test data in local storage */
+  var endpoint = 'a',
+    debug = 'b',
+    our_reference = 'c',
+    dhl_service = 'd',
+    goods_item = 'e',
+    headers = '{"a": "b"}';
 
-test('loadOptions', function(){
-	/* Set test data in local storage */
-	var endpoint = 'a',
-		debug = 'b',
-		our_reference = 'c',
-		dhl_service = 'd',
-		goods_item = 'e',
-		headers = '{"a": "b"}';
+  localStorage.endpoint = endpoint;
+  localStorage.debug = debug;
+  localStorage.headers = headers;
+  localStorage.our_reference = our_reference;
+  localStorage.dhl_service = dhl_service;
+  localStorage.goods_item = goods_item;
 
-	localStorage['endpoint'] = endpoint;
-	localStorage['debug'] = debug;
-	localStorage['headers'] = headers;
-	localStorage['our_reference'] = our_reference;
-	localStorage['dhl_service'] = dhl_service;
-	localStorage['goods_item'] = goods_item;
+  var response = options.load();
+  var expected_response = {endpoint: endpoint, debug: debug, headers: headers, our_reference: our_reference, dhl_service: dhl_service, goods_item: goods_item};
 
-	var response = options.load();
-	var expected_response = {endpoint: endpoint, debug: debug, headers: headers, our_reference: our_reference, dhl_service: dhl_service, goods_item: goods_item};
-
-	deepEqual(response, expected_response, 'Local storage data is loaded correctly');
+  assert.deepEqual(response, expected_response, 'Local storage data is loaded correctly');
 });
 
 
-test('setEndpoint', function(){
-	var endpoint = 'a';
-	var response = options.setEndpoint(endpoint);
-	equal(endpoint, response, 'Endpoint is stored correctly in Local storage');
+QUnit.test('setEndpoint', function (assert) {
+  var endpoint = 'a';
+  var response = options.setEndpoint(endpoint);
+  assert.equal(endpoint, response, 'Endpoint is stored correctly in Local storage');
 });
 
 
-test('setDebug', function(){
-	var expected = 'true';
-	var response = options.setDebugFlag(true);
-	equal(expected, response, 'Debug flag is stored correctly in Local storage');
+QUnit.test('setDebug', function (assert) {
+  var expected = 'true';
+  var response = options.setDebugFlag(true);
+  assert.equal(expected, response, 'Debug flag is stored correctly in Local storage');
 });
 
-test('setOurReference', function(){
-	var expected = 'dave';
-	var response = options.setOurReference('dave');
-	equal(expected, response, 'Our reference is stored correctly in Local storage');
+QUnit.test('setOurReference', function (assert) {
+  var expected = 'dave';
+  var response = options.setOurReference('dave');
+  assert.equal(expected, response, 'Our reference is stored correctly in Local storage');
 });
 
-test('setGoodsItem', function(){
-	var expected = 'Nerf';
-	var response = options.setGoodsItem('Nerf');
-	equal(expected, response, 'Goods item is stored correctly in Local storage');
+QUnit.test('setGoodsItem', function (assert) {
+  var expected = 'Nerf';
+  var response = options.setGoodsItem('Nerf');
+  assert.equal(expected, response, 'Goods item is stored correctly in Local storage');
 });
 
--- test/js/popup.js
+++ test/js/popup.js
@@ -1,98 +1,97 @@
-
+/*global QUnit: false, popup: false */
 module('popup');
 
 
-test('target_urls', function() {
-	var urls = popup.target_urls;
-	var test_urls = {register_shipment: 'http://www.dhlmultishipping.se/transnet/register.do?method=view', add_address: 'http://www.dhlmultishipping.se/transnet/organisation.jsp?organisation=new'};
+QUnit.test('target_urls', function (assert) {
+  var urls = popup.target_urls;
+  var test_urls = {register_shipment: 'http://www.dhlmultishipping.se/transnet/register.do?method=view', add_address: 'http://www.dhlmultishipping.se/transnet/organisation.jsp?organisation=new'};
 
-	deepEqual(urls, test_urls, 'DHL URLs setup defined correctly');
+  assert.deepEqual(urls, test_urls, 'DHL URLs setup defined correctly');
 });
 
 
-test('parseResposetextToCustomers', function() {
-	var test_data = [
-	  {
-	    "id": "1",
-	    "name": "Jesper Svensson",
-	    "address": "Street address",
-	    "zipcode": "12345",
-	    "city": "Växjö",
-	    "email": "svejes@gmail.com",
-	    "phone": "1029384746",
-	    "shipping": "DHL Servicepoint"
-	  },
-	  {
-	    "id": "2",
-	    "name": "Bengt Bengtsson",
-	    "address": "Bengtsvägen 790",
-	    "zipcode": "12345",
-	    "city": "Knäckebrödhult",
-	    "email": "",
-	    "phone": "012345678",
-	    "shipping": "DHL Paket"
-	  }
-	];
+QUnit.test('parseResposetextToCustomers', function (assert) {
+  var test_data = [
+    {
+      "id": "1",
+      "name": "Jesper Svensson",
+      "address": "Street address",
+      "zipcode": "12345",
+      "city": "Växjö",
+      "email": "svejes@gmail.com",
+      "phone": "1029384746",
+      "shipping": "DHL Servicepoint"
+    },
+    {
+      "id": "2",
+      "name": "Bengt Bengtsson",
+      "address": "Bengtsvägen 790",
+      "zipcode": "12345",
+      "city": "Knäckebrödhult",
+      "email": "",
+      "phone": "012345678",
+      "shipping": "DHL Paket"
+    }
+  ];
 
-	var test_string = JSON.stringify(test_data);
-	var expected_response = {};
-		expected_response[1] = test_data[0];
-		expected_response[2] = test_data[1];
+  var test_string = JSON.stringify(test_data);
+  var expected_response = {};
+  expected_response[1] = test_data[0];
+  expected_response[2] = test_data[1];
 
-	var response = popup.parseResposetextToCustomers(test_string);
+  var response = popup.parseResposetextToCustomers(test_string);
 
-	deepEqual(response, expected_response, 'JSON string is formatted into an expected object structure with id as index');
+  assert.deepEqual(response, expected_response, 'JSON string is formatted into an expected object structure with id as index');
 });
 
 
-test('getCustomerById', function() {
+QUnit.test('getCustomerById', function (assert) {
 
-	var test_data = [
-	  {
-	    "id": "1",
-	    "name": "Jesper Svensson",
-	    "address": "Street address",
-	    "zipcode": "12345",
-	    "city": "Växjö",
-	    "email": "svejes@gmail.com",
-	    "phone": "1029384746",
-	    "shipping": "DHL Servicepoint"
-	  },
-	  {
-	    "id": "2",
-	    "name": "Bengt Bengtsson",
-	    "address": "Bengtsvägen 790",
-	    "zipcode": "12345",
-	    "city": "Knäckebrödhult",
-	    "email": "",
-	    "phone": "012345678",
-	    "shipping": "DHL Servicepoint"
-	  }
-	];
+  var test_data = [
+    {
+      "id": "1",
+      "name": "Jesper Svensson",
+      "address": "Street address",
+      "zipcode": "12345",
+      "city": "Växjö",
+      "email": "svejes@gmail.com",
+      "phone": "1029384746",
+      "shipping": "DHL Servicepoint"
+    },
+    {
+      "id": "2",
+      "name": "Bengt Bengtsson",
+      "address": "Bengtsvägen 790",
+      "zipcode": "12345",
+      "city": "Knäckebrödhult",
+      "email": "",
+      "phone": "012345678",
+      "shipping": "DHL Servicepoint"
+    }
+  ];
 
-	var test_string = JSON.stringify(test_data);
-	var expected_response = test_data[1];
-			
-	/* populate popup.customers with our test data */
-	popup.customers = popup.parseResposetextToCustomers(test_string);
-	
+  var test_string = JSON.stringify(test_data);
+  var expected_response = test_data[1];
+
+  /* populate popup.customers with our test data */
+  popup.customers = popup.parseResposetextToCustomers(test_string);
   var response = popup.getCustomerById(2);
-	deepEqual(expected_response, response, 'Customer with ID 2 is returned');
+  assert.deepEqual(expected_response, response, 'Customer with ID 2 is returned');
 });
 
 
-test('getShippingServiecByNameDHLPaket', function() {
+QUnit.test('getShippingServiecByNameDHLPaket', function (assert) {
 
-	var shipping_name = 'DHL Paket', expected_response = 182875;
-	var response = popup.getShippingServiecByName(shipping_name);		
+  var shipping_name = 'DHL Paket', expected_response = 182875;
+  var response = popup.getShippingServiecByName(shipping_name);
 
-	equal(expected_response, response, 'Shipping service for DHL Paket was returned');	
+  assert.equal(expected_response, response, 'Shipping service for DHL Paket was returned');
 });
 
-test('getShippingServiecByNameDHLServicepoint', function() {
+QUnit.test('getShippingServiecByNameDHLServicepoint', function (assert) {
 
-	var shipping_name = 'DHL Servicepoint', expected_response = 182880;
-	var response = popup.getShippingServiecByName(shipping_name);		
+  var shipping_name = 'DHL Servicepoint', expected_response = 182880;
+  var response = popup.getShippingServiecByName(shipping_name);
 
-	equal(expected_response, response, 'Shipping service for DHL Servicepoint was returned');	
+  assert.equal(expected_response, response, 'Shipping service for DHL Servicepoint was returned');
 });
